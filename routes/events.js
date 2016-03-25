b92429768e99dc87e50d58b3da3c8960a0fd06d9
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-03-25 11:38:42 +0100

    require login for create, delete and update event actions

--- routes/events.js
+++ routes/events.js
@@ -2,10 +2,20 @@ var router = require('express').Router();
 
 module.exports = function(server){
     router.get('/', server.actions.events.get);
-    router.post('/', server.middlewares.bodyparser, server.actions.events.create);
+    router.post('/',
+        server.middlewares.bodyparser,
+        server.middlewares.ensureAuthenticated,
+        server.actions.events.create
+    );
     router.get('/:id', server.actions.events.show);
-    router.put('/:id', server.middlewares.bodyparser, server.actions.events.update);
-    router.delete('/:id', server.actions.events.remove);
+    router.put('/:id',
+        server.middlewares.bodyparser,
+        server.middlewares.ensureAuthenticated,
+        server.actions.events.update);
+    router.delete('/:id',
+        server.middlewares.ensureAuthenticated,
+        server.actions.events.remove
+    );
 
     return router;
 }
