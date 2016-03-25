70c746f7a03f05d531788d21774464c4d0bbea63
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-03-25 15:09:48 +0100

    remove console.log

--- actions/events/list.js
+++ actions/events/list.js
@@ -1,14 +1,11 @@
 function getFutureEvents(server){
     return function(req, res, next){
-
         var Event = server.models.Event;
-        console.log("apres eventttttttttttttttt");
+
         Event.find({date:{"$gte": new Date()}
             }, function(err, events){
-                if (err){
-                    console.log("errorsssssssssssssss");
+                if (err)
                     res.status(500).send(err);
-                }
 
             res.send(events);
         });
