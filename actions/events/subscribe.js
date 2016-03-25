189164a8e78f50abcdfb50d2f92e1f8dbd0c0c0b
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-03-25 14:25:06 +0100

    subscribe user to event

---
+++ actions/events/subscribe.js
@@ -0,0 +1,28 @@
+module.exports = function(server){
+    return function (req, res, next){
+        var Event = server.models.Event;
+        var User = server.models.User;
+
+        Event.findOne({ '_id': req.params.id }, function(err, currentEvent){
+            if (err)
+                return res.status(500).send(err)
+            if (!currentEvent)
+                return res.status(404).send('event not found')
+
+            User.findOne({ _id: req.auth.userId }, function(err, user){
+                if (err)
+                    return res.status(500).send(err)
+
+                currentEvent.populate('participants');
+                currentEvent.participants.push(user);
+
+                currentEvent.save(function(err, data){
+                    if (err)
+                        return res.status(500).send(err)
+
+                    res.send(currentEvent);
+                });
+            });
+        });
+    }
+}
