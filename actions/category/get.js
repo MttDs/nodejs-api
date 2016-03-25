498d63cae8f83914550a408dfa250f423d28d8a0
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-03-25 11:06:14 +0100

    listing categories from mongo

--- actions/category/get.js
+++ actions/category/get.js
@@ -1,5 +1,12 @@
 module.exports = function(server){
     return function(req, res, next){
-        res.send('ok');
+        var Category = server.models.Category;
+
+        Category.find({}, function(err, categories) {
+            if (err)
+                return res.status(500).send(err);
+
+            res.send(categories)
+        });
     }
 }
